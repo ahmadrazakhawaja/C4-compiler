29c1f7edc03af5b1a3a80c112b2346da4098f566
ahmadrazakhawaja <ahmad@sidestream.tech>
2026-01-22 23:27:17 +0100

    fix for semantics

--- src/semantic/Semantic.cpp
+++ src/semantic/Semantic.cpp
@@ -21,6 +21,7 @@ struct Type {
     std::shared_ptr<Type> pointee;
     std::shared_ptr<Type> returnType;
     std::vector<Type> params;
+    bool paramsKnown = true;
 };
 
 static Type makeError() { return Type{Type::Kind::Error}; }
@@ -34,6 +35,14 @@ static Type makeFunction(const Type& ret, const std::vector<Type>& params) {
     t.kind = Type::Kind::Function;
     t.returnType = std::make_shared<Type>(ret);
     t.params = params;
+    t.paramsKnown = true;
+    return t;
+}
+static Type makeFunctionUnspecified(const Type& ret) {
+    Type t;
+    t.kind = Type::Kind::Function;
+    t.returnType = std::make_shared<Type>(ret);
+    t.paramsKnown = false;
     return t;
 }
 
@@ -58,6 +67,7 @@ static bool typeEqual(const Type& a, const Type& b) {
             return typeEqual(*a.pointee, *b.pointee);
         case Type::Kind::Function:
             if (!typeEqual(*a.returnType, *b.returnType)) return false;
+            if (!a.paramsKnown || !b.paramsKnown) return true;
             if (a.params.size() != b.params.size()) return false;
             for (size_t i = 0; i < a.params.size(); ++i) {
                 if (!typeEqual(a.params[i], b.params[i])) return false;
@@ -261,6 +271,10 @@ private:
         for (const auto& plist : params) {
             if (plist.isArray) {
                 current = makePointer(current);
+                continue;
+            }
+            if (plist.params.empty()) {
+                current = makeFunctionUnspecified(current);
                 continue;
             }
             if (isVoidOnlyParamList(plist)) {
@@ -616,17 +630,23 @@ private:
                     info.type = makeError();
                     return info;
                 }
-                if (node->getChildren().size() - 1 != funcType.params.size()) {
-                    report(info.loc, "argument count mismatch");
-                }
-                for (size_t i = 1; i < node->getChildren().size(); ++i) {
-                    ExprInfo arg = analyzeExpr(node->getChildren().at(i));
-                    if (i - 1 < funcType.params.size()) {
-                        const Type& paramType = funcType.params[i - 1];
-                        if (!valueCompatible(paramType, arg.type) &&
-                            !(isPointer(paramType) && arg.isNullPtrConst)) {
-                            report(info.loc, "argument type mismatch");
+                if (funcType.paramsKnown) {
+                    if (node->getChildren().size() - 1 != funcType.params.size()) {
+                        report(info.loc, "argument count mismatch");
+                    }
+                    for (size_t i = 1; i < node->getChildren().size(); ++i) {
+                        ExprInfo arg = analyzeExpr(node->getChildren().at(i));
+                        if (i - 1 < funcType.params.size()) {
+                            const Type& paramType = funcType.params[i - 1];
+                            if (!valueCompatible(paramType, arg.type) &&
+                                !(isPointer(paramType) && arg.isNullPtrConst)) {
+                                report(info.loc, "argument type mismatch");
+                            }
                         }
+                    }
+                } else {
+                    for (size_t i = 1; i < node->getChildren().size(); ++i) {
+                        analyzeExpr(node->getChildren().at(i));
                     }
                 }
                 info.type = *funcType.returnType;
